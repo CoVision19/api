ffe6f3f068ace164991d4f96f3c3cbc3ce36dc0f
Thomas Hedoux <thomas.hedoux@epitech.eu>
2020-04-25 10:46:30 +0200

    wip: begining of a hash table to not parse a day if not necessary

--- routes/home.js
+++ routes/home.js
@@ -11,7 +11,12 @@ module.exports = ({ router }) => {
 			message: (controller.CacheController.GetCache().lastTimeFetched === null ? 'CoVision19 API up and running! No data fetched yet.' : 'CoVision19 API up and running! Data available.'),
 			date: date,
 			time: time,
-			lastCacheUpdate: controller.CacheController.GetCache().lastTimeFetched
+			lastCacheUpdate: controller.CacheController.GetCache().lastTimeFetched,
+			checksums: controller.CacheController.GetCache().checksums
 		};
+	});
+
+	router.get('/update', (ctx, next) => {
+		controller.CacheController.GetCache().refreshCache();
 	});
 }
