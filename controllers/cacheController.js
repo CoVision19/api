c1bcccc28643a7b6b0f4524bd91249f422ec8238
Thomas Hedoux <thomas.hedoux@epitech.eu>
2020-04-25 10:46:02 +0200

    feat: inserted promises for parsing, so everything is ran in parallel

--- controllers/cacheController.js
+++ controllers/cacheController.js
@@ -1,5 +1,6 @@
 const Got = require('got');
-const csv = require("csvtojson");
+const csv = require('csvtojson');
+const Crypto = require('crypto');
 
 class CacheController {
 	constructor(startYear, startMonth, startDay) {
@@ -8,6 +9,8 @@ class CacheController {
 		this.lastTimeFetched = null;
 		this.data = {};
 		this.lookupTable = {};
+		this.checksums = {};
+		this.startUID = 90000000;
 	}
 
 	// Singleton functions
@@ -28,8 +31,7 @@ class CacheController {
 		var dateObj = new Date(dateStr);
 
 		// If date is invalid, dateObj.getTime() returns NaN, which is never equal to itself
-        //
-        return (dateStr.match(/\d{4}-\d{2}-\d{2}/) && dateObj.getTime() === dateObj.getTime());
+		return (dateStr.match(/\d{4}-\d{2}-\d{2}/) && dateObj.getTime() === dateObj.getTime());
 	}
 
 	isDateInCache(dateStr) {
@@ -41,121 +43,151 @@ class CacheController {
 	}
 
 	refreshCache() {
-		var tmpDate = new Date(this.startDate);
-		var today = new Date();
-		var promises = [];
-		var lookupPromise;
-		var startUID = 90000000;
+		let tmpDate = new Date(this.startDate);
+		let today = new Date();
+		let promises = [];
+		let lookupPromise;
 
 		lookupPromise = this.fetchLookupTable();
 		lookupPromise.then(async (res) => {
 			if (res.content !== null) {
-				this.lookupTable = await csv({ignoreColumns: /(iso2|iso3|code3)/}).fromString(res.content);
+				this.lookupTable = await csv({ ignoreColumns: /(iso2|iso3|code3)/ }).fromString(res.content);
 
 				console.log('Fetching COVID-19 data from ' + tmpDate + ' to ' + today + '...');
 				while (tmpDate < today) {
-					promises.push(this.myFetch(this.formatDate(tmpDate)));
+					promises.push(this.myFetch(this.formatDateISOtoMDY(tmpDate)));
 					tmpDate.setUTCDate(tmpDate.getUTCDate() + 1);
 				}
 				Promise.all(promises).then(async res => {
-					await this.awaitForEach(res, async (elem) => {
-						var splitDate = elem.date.split('-');
-						var dateKey = splitDate[2] + '-' + splitDate[0] + '-' + splitDate[1];
-						if (elem.content !== null || !(this.data[dateKey])) {
-							//var columns = /(FIPS|Admin2|Province_State|Country_Region|Last_Update|Combined_Key|Province\/State|Country\/Region|Last\ Update)/
-							this.data[dateKey] = (elem.content === null ? null : await csv().fromString(elem.content));
-							if (this.data[dateKey] !== null) {
-								this.data[dateKey].forEach( region => {
-									this.lookupTable.forEach( entry => {
-										if ((this.isAlmostEqual(entry.Lat, region.Lat) && this.isAlmostEqual(entry.Long_, region.Long_)) ||
-											(this.isAlmostEqual(entry.Lat, region.Latitude) && this.isAlmostEqual(entry.Long_, region.Longitude))) {
-											region.UID = entry.UID;
-										}
-									});
-									if (!region.UID) {
-										var newRegion = {
-											UID: startUID,
-											Lat: region.Lat || region.Latitude || '',
-											Long_: region.Long_ || region.Longitude || '',
-											FIPS: region.FIPS || '',
-											Admin2: region.Admin2 || '',
-											Province_State: region.Province_State || region['Province/State'] || '',
-											Country_Region: region.Country_Region || region['Country/Region'] || '',
-											Combined_Key: region.Combined_Key || ''
-										}
-										this.lookupTable.push(newRegion);
-										region.UID = toString(startUID);
-										startUID += 1;
-									}
-									delete region.Lat;
-									delete region.Long_;
-									delete region.Latitude;
-									delete region.Longitude;
-									delete region.FIPS;
-									delete region.Admin2;
-									delete region.Province_State;
-									delete region.Country_Region;
-									delete region.Last_Update;
-									delete region.Combined_Key;
-									delete region['Province/State'];
-									delete region['Country/Region'];
-									delete region['Last Update'];
-								});
-							}
-						}
-						console.log('Parsed: ' + dateKey);
+					let parsingPromises = [];
+					res.forEach(elem => {
+						parsingPromises.push(this.createParsingPromise(elem));
 					});
-
-					this.lastTimeFetched = new Date();
-					console.log('Cache built at ' + this.lastTimeFetched + '!\n');
+					Promise.all(parsingPromises).then(res => {
+						this.lastTimeFetched = new Date();
+						console.log('Cache built at ' + this.lastTimeFetched + '!\n');
+					}).catch(err => {
+						console.log('AN ERROR OCCURRED WHILE PARSING THE CACHE: ' + err);
+					});
 				}).catch(err => {
 					console.log('AN ERROR OCCURRED WHILE BUILDING THE CACHE: ' + err);
-				})
+				});
 			}
 		}).catch(err => {
 			console.log('AN ERROR OCCURRED WHILE BUILDING THE LOOKUP TABLE: ' + err);
-		})
+		});
 	}
 
-	async myFetch(dateString) {
+	createParsingPromise(dayData) {
+		return new Promise(async (resolve, reject) => {
+			await this.parseDay(resolve, reject, dayData);
+		});
+	}
+
+	async parseDay(resolve, reject, elem) {
+		if (elem.content !== null || !(this.data[elem.date])) {
+			this.data[elem.date] = (elem.content === null ? null : await csv().fromString(elem.content));
+			if (this.data[elem.date] !== null) {
+				this.data[elem.date].forEach(region => {
+					this.lookupTable.forEach(entry => {
+						if ((this.isAlmostEqual(entry.Lat, region.Lat) && this.isAlmostEqual(entry.Long_, region.Long_)) ||
+							(this.isAlmostEqual(entry.Lat, region.Latitude) && this.isAlmostEqual(entry.Long_, region.Longitude))) {
+							region.UID = entry.UID;
+						}
+					});
+					if (!region.UID) {
+						var newRegion = {
+							UID: this.startUID,
+							Lat: region.Lat || region.Latitude || '',
+							Long_: region.Long_ || region.Longitude || '',
+							FIPS: region.FIPS || '',
+							Admin2: region.Admin2 || '',
+							Province_State: region.Province_State || region['Province/State'] || '',
+							Country_Region: region.Country_Region || region['Country/Region'] || '',
+							Combined_Key: region.Combined_Key || ''
+						}
+						this.lookupTable.push(newRegion);
+						region.UID = toString(this.startUID);
+						this.startUID += 1;
+					}
+					delete region.Lat;
+					delete region.Long_;
+					delete region.Latitude;
+					delete region.Longitude;
+					delete region.FIPS;
+					delete region.Admin2;
+					delete region.Province_State;
+					delete region.Country_Region;
+					delete region.Last_Update;
+					delete region.Combined_Key;
+					delete region['Province/State'];
+					delete region['Country/Region'];
+					delete region['Last Update'];
+				});
+			}
+			console.log('Parsed: ' + elem.date);
+			resolve(null);
+		} else {
+			console.log('Ignored: ' + elem.date);
+			resolve(null);
+		}
+	}
+
+	myFetch(dateString) {
+		let date = this.formatDateMDYtoYMD(dateString);
 		let prom = new Promise((resolve, reject) => {
 			Got(this.baseURL + 'csse_covid_19_daily_reports/' + dateString + '.csv').then(res => {
-				console.log('Fetched: ' + dateString);
-				resolve({date: dateString, content: res.body});
-			})
-			.catch(err => {
-				console.log('Reject (' + err.response.body.replace(/(\r\n|\n|\r)/gm,"") + '): ' + dateString);
-				resolve({date: dateString, content: null});
-			})
-		})
+				console.log('Fetched: ' + date);
+				if (res.body && !this.isHashSame(date, res.body)) {
+					this.addHash(date, JSON.stringify(res.body));
+					resolve({ date: date, content: res.body });
+				}
+				resolve({ date: date, content: null });
+			}).catch(err => {
+				console.log('Reject (' + err.response.body.replace(/(\r\n|\n|\r)/gm, "") + '): ' + dateString);
+				resolve({ date: date, content: null });
+			});
+		});
 		return prom;
 	}
 
 	fetchLookupTable() {
-		console.log('Fetching UID lookup table...'); 
+		console.log('Fetching UID lookup table...');
 		let prom = new Promise((resolve, reject) => {
 			Got(this.baseURL + 'UID_ISO_FIPS_LookUp_Table.csv').then(res => {
-				resolve({content: res.body});
-			})
-			.catch(err => {
-				console.log('Reject (' + err.response.body.replace(/(\r\n|\n|\r)/gm,"") + ')');
-				resolve({content: null});
-			})
-		})
-
+				resolve({ content: res.body });
+			}).catch(err => {
+				console.log('Reject (' + err.response.body.replace(/(\r\n|\n|\r)/gm, "") + ')');
+				resolve({ content: null });
+			});
+		});
 		return prom;
 	}
 
-	formatDate(date) {
+	formatDateMDYtoYMD(date) {
+		var splitDate = date.split('-');
+		return splitDate[2] + '-' + splitDate[0] + '-' + splitDate[1];
+	}
+
+	formatDateISOtoMDY(date) {
 		var splitter = date.toISOString().split('T')[0].split('-');
 		return splitter[1] + '-' + splitter[2] + '-' + splitter[0];
 	}
 
 	async awaitForEach(array, callback) {
 		for (let index = 0; index < array.length; index++) {
-		  await callback(array[index], index, array);
+			await callback(array[index], index, array);
 		}
-	  }
+	}
+
+	addHash(date, str) {
+		this.checksums[date] = Crypto.createHash('sha1').update(str).digest('hex');
+	}
+
+	isHashSame(date, str) {
+		let hashToCheck = Crypto.createHash('sha1').update(str).digest('hex');
+		return (date in this.checksums && this.checksums[date] === hashToCheck);
+	}
 }
 
 module.exports = {
