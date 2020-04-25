d95fd2b9ddf53aa144cedd77fb212a6a771e8f3f
Thomas Hedoux <thomas.hedoux@epitech.eu>
2020-04-25 10:56:37 +0200

    feat: body response are hashed and compare so it is not parsed if the data is already present. Saving cputime

--- controllers/cacheController.js
+++ controllers/cacheController.js
@@ -138,7 +138,9 @@ class CacheController {
 		let prom = new Promise((resolve, reject) => {
 			Got(this.baseURL + 'csse_covid_19_daily_reports/' + dateString + '.csv').then(res => {
 				console.log('Fetched: ' + date);
-				if (res.body && !this.isHashSame(date, res.body)) {
+				// DEBUG
+				console.log('HASHING part:' + this.isHashSame(date, JSON.stringify(res.body)));
+				if (res.body && !this.isHashSame(date, JSON.stringify(res.body))) {
 					this.addHash(date, JSON.stringify(res.body));
 					resolve({ date: date, content: res.body });
 				}
@@ -185,7 +187,12 @@ class CacheController {
 	}
 
 	isHashSame(date, str) {
+		if (!str || str === null)
+			return false;
 		let hashToCheck = Crypto.createHash('sha1').update(str).digest('hex');
+		// DEBUG
+		console.log("NEW HASH:" + hashToCheck);
+		console.log("EXISTING HASH IN TABLE: " + this.checksums[date]);
 		return (date in this.checksums && this.checksums[date] === hashToCheck);
 	}
 }
